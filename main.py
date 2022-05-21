cee0cc44e8a5b2660e70141dd4ede42a3573b594
Ratanraj Ravuri <ratanraj.r@gmail.com>
2022-05-22 02:45:31 +0530

    monster

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 import pygame
 from pygame.locals import *
+from levels import levels
 
-class Game():
+# TODO: Fix the line drawing algorithm
+def bresenham(x1,y1,x2, y2):
+    m_new = 2 * (y2 - y1)
+    slope_error_new = m_new - (x2 - x1)
+    y=y1
+    for x in range(x1,x2+1, -1 if x1>x2 else 1):
+        #print("(",x ,",",y ,")\n")
+        yield (x,y)
+        slope_error_new =slope_error_new + m_new
+        if (slope_error_new >= 0):
+            y=y+1
+            slope_error_new =slope_error_new - 2 * (x2 - x1)
+
+
+
+def extrapolate_walk_path(walk_path):
+    for i in range(0, len(walk_path)-1, 1):
+        x1,y1,x2,y2 = walk_path[i][0], walk_path[i][1], walk_path[i+1][0], walk_path[i+1][1]
+        for p in bresenham(x1, y1, x2, y2):
+            yield p
+
+
+class Boogy:
+    def __init__(self, walk_path) -> None:
+        self.path = extrapolate_walk_path(walk_path=walk_path)
+
+    def plot(self, screen):
+        screen.blit(self.sprite, next(self.path))
+
+
+class Monster1(Boogy):
+    def __init__(self, walk_path) -> None:
+        self.sprite = pygame.image.load('monster_small.png')
+        super().__init__(walk_path)
+
+
+class Game:
     def __init__(self) -> None:
         pygame.init()
         self.fpsClock = pygame.time.Clock()
@@ -11,6 +48,7 @@ class Game():
 
     def load_images(self):
         self.images['splash'] = pygame.image.load('splash.png')
+        self.images['level1'] = pygame.image.load('levels/level1.png')
 
     def splash_screen(self):
         running = True
@@ -36,19 +74,29 @@ class Game():
             pygame.display.update()
             self.fpsClock.tick(12)
 
-            
+    def load_level(self, n):
+        for i in levels:
+            if i['id'] == n:
+                return i
 
     def main(self):
         pygame.init()
         self.load_images()
 
         self.splash_screen()
-        self.level_selector()
+        #selected_level = self.level_selector()
+        selected_level = 1
+
+        level = self.load_level(selected_level)
+
+        print(f'Level: {level}')
 
         running = True
+        m1 = Monster1(level['path'])
         while running:
             self.screen.fill(pygame.Color(255, 0, 0))
-
+            self.screen.blit(self.images[level['map']], (0, 0))
+            m1.plot(self.screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
